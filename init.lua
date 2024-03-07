8e15de7d05208dc4ec710a45582ff25a7725ab14
Adrienne <adrienne.unsworth@gmail.com>
2024-03-07 13:50:38 +0000

    fix?

--- init.lua
+++ init.lua
@@ -817,13 +817,8 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-<<<<<<< HEAD
-  --    For additional information see: :help lazy.nvim-lazy.nvim-structuring-your-plugins
-  { import = 'custom.plugins' },
-}, {})
-=======
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+   { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you have a Nerd Font, set icons to an empty table which will use the
@@ -845,7 +840,6 @@ require('lazy').setup({
     },
   },
 })
->>>>>>> 66e2a5a425a4f3b2e4f9456d82b29718c24a8993
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
